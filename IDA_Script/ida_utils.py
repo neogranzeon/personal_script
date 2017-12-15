61fe0edf60998fce373c0235d47076eef3a7611f
lichao26 <lichao26@baidu.com>
2017-12-15 18:23:39 +0800

    update automatic

--- IDA_Script/ida_utils.py
+++ IDA_Script/ida_utils.py
@@ -67,3 +67,36 @@ def AddCFStringRef():
         cfstring = begin + i * 16
         cstring = Dword(cfstring + 8)
         AddCodeXref(cfstring, cstring, XREF_USER | fl_F)
+		
+def SetTypeOnObjCFunc():
+	# 自动标注Objective-C函数参数
+    segb, sege = GetSegRange('__text')
+    segb -= 1
+    while NextFunction(segb) != BADADDR:
+        faddr = NextFunction(segb)
+        funcsize = GetFunctionAttr(faddr, FUNCATTR_END) - faddr
+        funcname = GetFunctionName(faddr)
+        if funcname.find('[') != -1:
+            # ObjC func
+            if funcname.find(':') != -1:
+                # find ObjC func which have params
+                params = funcname.split(' ')[1].split(':')
+                origintype = GetType(faddr);
+                if origintype is not None:
+                    origintype= origintype.split(',')
+                    origintype[0] = origintype[0].replace('__cdecl','f')
+                    if len(origintype) - 2 <= len(params): 
+                        for i in range(2, len(origintype)):
+                            if i == len(origintype) - 1:
+                                origintype[i] = origintype[i].replace(')', '')
+                            params[i - 2] = params[i - 2].replace('set', '')
+                            origintype[i] += ' ' + params[i - 2].lower()
+                            if i == len(origintype) - 1:
+                                origintype[i]  += ')'
+                        newtype = ','.join(origintype)
+                        SetType(faddr, newtype)
+                else:
+                    print 'sp:%x' % addr
+        segb = faddr + funcsize - 1
+
+		
