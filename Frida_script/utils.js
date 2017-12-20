6970235a0ea7ceeb2cc9f3461b06435606c54b7b
lichao26 <lichao26@baidu.com>
2017-12-20 16:20:28 +0800

    update randomString

--- Frida_script/utils.js
+++ Frida_script/utils.js
@@ -170,3 +170,15 @@ function forcetrustcert() {
 		});
 	}
 }
+
+// 随机字符串
+function randomString(len) {
+　　len = len || 32;
+　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+　　var maxPos = $chars.length;
+　　var pwd = '';
+　　for (i = 0; i < len; i++) {
+　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return pwd;
+}
